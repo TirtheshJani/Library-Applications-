527f37578494d7aca9d437df3ae08bb3ecb11672
Tirthesh Jani <tirtheshjani@gmail.com>
2025-05-24 12:14:38 -0400

    new Oauth integration for google

--- booksearch/app.py
+++ booksearch/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
+from flask import Flask, render_template, redirect, url_for, flash, request, jsonify, session, current_app, abort
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash
 import requests
+import secrets
+from urllib.parse import urlencode
 from config import Config
 from models import db, User, BookSearch, SavedBook
 import json
@@ -117,6 +119,110 @@ def login():
             flash('Invalid username or password', 'error')
     
     return render_template('login.html')
+
+# OAuth Routes
+@app.route('/authorize/<provider>')
+def oauth2_authorize(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('search'))
+
+    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
+    if provider_data is None:
+        abort(404)
+
+    # generate a random string for the state parameter
+    session['oauth2_state'] = secrets.token_urlsafe(16)
+
+    # create a query string with all the OAuth2 parameters
+    qs = urlencode({
+        'client_id': provider_data['client_id'],
+        'redirect_uri': url_for('oauth2_callback', provider=provider, _external=True),
+        'response_type': 'code',
+        'scope': ' '.join(provider_data['scopes']),
+        'state': session['oauth2_state'],
+    })
+
+    # redirect the user to the OAuth2 provider authorization URL
+    return redirect(provider_data['authorize_url'] + '?' + qs)
+
+@app.route('/callback/<provider>')
+def oauth2_callback(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('search'))
+
+    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
+    if provider_data is None:
+        abort(404)
+
+    # if there was an authentication error, flash the error messages and exit
+    if 'error' in request.args:
+        for k, v in request.args.items():
+            if k.startswith('error'):
+                flash(f'{k}: {v}', 'error')
+        return redirect(url_for('login'))
+
+    # make sure that the state parameter matches the one we created in the
+    # authorization request
+    if request.args.get('state') != session.get('oauth2_state'):
+        abort(401)
+
+    # make sure that the authorization code is present
+    if 'code' not in request.args:
+        abort(401)
+
+    # exchange the authorization code for an access token
+    response = requests.post(provider_data['token_url'], data={
+        'client_id': provider_data['client_id'],
+        'client_secret': provider_data['client_secret'],
+        'code': request.args['code'],
+        'grant_type': 'authorization_code',
+        'redirect_uri': url_for('oauth2_callback', provider=provider, _external=True),
+    }, headers={'Accept': 'application/json'})
+    
+    if response.status_code != 200:
+        abort(401)
+    
+    oauth2_token = response.json().get('access_token')
+    if not oauth2_token:
+        abort(401)
+
+    # use the access token to get the user's email address
+    response = requests.get(provider_data['userinfo']['url'], headers={
+        'Authorization': 'Bearer ' + oauth2_token,
+        'Accept': 'application/json',
+    })
+    
+    if response.status_code != 200:
+        abort(401)
+    
+    email = provider_data['userinfo']['email'](response.json())
+
+    # find or create the user in the database
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        # Create new user from OAuth
+        username = email.split('@')[0]  # Use email prefix as username
+        
+        # Make sure username is unique
+        counter = 1
+        original_username = username
+        while User.query.filter_by(username=username).first():
+            username = f"{original_username}_{counter}"
+            counter += 1
+        
+        user = User(email=email, username=username)
+        # For OAuth users, we don't set a password
+        user.password_hash = ''  # Empty password hash for OAuth users
+        db.session.add(user)
+        db.session.commit()
+        
+        flash(f'Welcome! Your account has been created via {provider.title()}.', 'success')
+    else:
+        flash(f'Welcome back! Logged in via {provider.title()}.', 'success')
+
+    # log the user in
+    login_user(user)
+    return redirect(url_for('search'))
 
 @app.route('/logout')
 @login_required
